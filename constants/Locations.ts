f2ee24eaa4ee637da10add9794ffa6503f0f5f4b
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2025-03-18 19:07:11 -0300

    Project initialization: basic settings and structure

---
+++ constants/Locations.ts
@@ -0,0 +1,38 @@
+// Pacote de locais: Natureza
+export const naturezaLocais = [
+  "Praia Tropical",
+  "Floresta Amazônica",
+  "Deserto do Saara",
+  "Montanhas dos Alpes",
+  "Cachoeira",
+  "Caverna Natural",
+  "Recife de Coral",
+  "Savana Africana",
+  "Geleira Ártica",
+  "Lago Cristalino",
+  "Vulcão Ativo",
+  "Ilha Deserta",
+  "Pântano",
+  "Cânion",
+  "Oásis no Deserto",
+]
+
+// Pacote de locais: Terror
+export const terrorLocais = [
+  "Cemitério Abandonado",
+  "Mansão Mal-Assombrada",
+  "Hospital Psiquiátrico",
+  "Floresta Sombria",
+  "Navio Fantasma",
+  "Catacumbas Antigas",
+  "Circo Abandonado",
+  "Laboratório Secreto",
+  "Cidade Fantasma",
+  "Hotel Amaldiçoado",
+  "Escola Abandonada",
+  "Caverna dos Monstros",
+  "Estação Espacial Infestada",
+  "Teia de Aranha Gigante",
+  "Parque de Diversões Abandonado",
+]
+
